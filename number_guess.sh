523c9299fe0ee5601b2ad0da33668271af79610b
FucciVanni2 great <akachamedamine@gmail.com>
2024-04-16 19:44:21 +0000

    feat: welcome username and fetch data

--- number_guess.sh
+++ number_guess.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 GUESS=$RANDOM
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+echo "Enter your username:"
+read USERNAME
 
-echo $GUESS
+PLAYER_USERNAME=$($PSQL "select username from players where username ilike "$USERNAME"")
+if [[ -z $PLAYER_USERNAME ]]
+then
+echo "Welcome, "$PLAYER_USERNAME"! It looks like this is your first time here."
+fi
