7dfefb7c20a6280976b7e1288b9de51da3d82c35
FucciVanni2 great <akachamedamine@gmail.com>
2024-04-16 21:12:53 +0000

    feat: almost done just polishing now

--- number_guess.sh
+++ number_guess.sh
@@ -8,27 +8,41 @@ PLAYER_USERNAME=$($PSQL "select username from players where username ilike '$USE
 if [[ -z $PLAYER_USERNAME ]]
 then
 echo "Welcome, $USERNAME! It looks like this is your first time here."
+INSERT_NEW_PLAYER=$($PSQL "INSERT INTO players(username, games_played) values('$USERNAME', 1)")
 else
 PLAYER_INFO=$($PSQL "select * from players where username ilike '$USERNAME'")
 echo "$PLAYER_INFO" | while IFS="|" read ID USER GAMES BEST 
+
 do
 echo "Welcome back, $USER! You have played $GAMES games, and your best game took $BEST guesses."
+INCREMENT_GAMES=$((GAMES+1))
+INSERT_NEW_GAME=$($PSQL "update players set games_played = $INCREMENT_GAMES where username = '$USERNAME'")
 done
 fi
 SECRET=$(( ( RANDOM % 1000 )  + 1 ))
 NUMBER_OF_TRIES=0
+ISITNUMBER='^[0-9]+$'
 
+echo "$SECRET"
 echo "Guess the secret number between 1 and 1000:"
-echo $SECRET
 read PLAYER_GUESS
-while [ $PLAYER_GUESS != $SECRET ]
-NUMBER_OF_TRIES=$((NUMBER_OF_TRIES+1))
+
+
+while [[ $PLAYER_GUESS -ne $SECRET ]]
 do
+if ! [[ $PLAYER_GUESS =~ $ISITNUMBER ]]
+then
+echo "That is not an integer, guess again:"
+NUMBER_OF_TRIES=$NUMBER_OF_TRIES
+read PLAYER_GUESS
+fi
+NUMBER_OF_TRIES=$((NUMBER_OF_TRIES+1))
 if [[ $PLAYER_GUESS -gt $SECRET ]]
 then
 echo "It's lower than that, guess again:" 
 read PLAYER_GUESS
-else
+elif [[ $PLAYER_GUESS -lt $SECRET ]]
+then
 echo "It's higher than that, guess again:"
 read PLAYER_GUESS
 fi
@@ -36,6 +50,15 @@ done
 
 if [[ $PLAYER_GUESS == $SECRET ]]
 then
-
+if [[ $NUMBER_OF_TRIES == 0 ]]
+then
+NUMBER_OF_TRIES=$((NUMBER_OF_TRIES+1))
+fi
 echo "You guessed it in $NUMBER_OF_TRIES tries. The secret number was $SECRET. Nice job!"
+SELECT_BEST_RECORD=$($PSQL "select best_game from players where username ilike '$USERNAME'")
+if [[ $SELECT_BEST_RECORD -gt $NUMBER_OF_TRIES ]] || [[ -z $SELECT_BEST_RECORD ]]
+then
+INSERT_RECORD=$($PSQL "update players set best_game = $NUMBER_OF_TRIES where username ilike '$USERNAME'")
+fi
+exit
 fi
