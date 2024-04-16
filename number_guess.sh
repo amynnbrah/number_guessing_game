169f09115b3a947d72e892e3159a4b2e2d4a8473
FucciVanni2 great <akachamedamine@gmail.com>
2024-04-16 20:20:44 +0000

    test: just testing loops for now

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-GUESS=$RANDOM
+
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo "Enter your username:"
 read USERNAME
@@ -15,4 +15,27 @@ do
 echo "Welcome back, $USER! You have played $GAMES games, and your best game took $BEST guesses."
 done
 fi
+SECRET=$(( ( RANDOM % 1000 )  + 1 ))
+NUMBER_OF_TRIES=0
+
 echo "Guess the secret number between 1 and 1000:"
+echo $SECRET
+read PLAYER_GUESS
+while [ $PLAYER_GUESS != $SECRET ]
+NUMBER_OF_TRIES=$((NUMBER_OF_TRIES+1))
+do
+if [[ $PLAYER_GUESS -gt $SECRET ]]
+then
+echo "It's lower than that, guess again:" 
+read PLAYER_GUESS
+else
+echo "It's higher than that, guess again:"
+read PLAYER_GUESS
+fi
+done
+
+if [[ $PLAYER_GUESS == $SECRET ]]
+then
+
+echo "You guessed it in $NUMBER_OF_TRIES tries. The secret number was $SECRET. Nice job!"
+fi
